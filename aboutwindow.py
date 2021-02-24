1ba75b1f6f09331752ab814cf45b018251940635
New1st <sergey.zvyagin@yahoo.com>
2021-02-24 17:18:26 +0700

    Создан холст

--- aboutwindow.py
+++ aboutwindow.py
@@ -18,14 +18,17 @@ class AboutWindow(tkinter.Toplevel):
         header_frame.pack(side=tkinter.TOP, fill=tkinter.X)
 
         self.image = tkinter.PhotoImage(file="GraphHelper.png")
-        logo = tkinter.Label(header_frame, bg="#f2f2f2", justify=tkinter.LEFT, \
+        logo = tkinter.Label(
+            header_frame, bg="#f2f2f2", justify=tkinter.LEFT,
             height=50, width=50, image=self.image)
         logo.pack(side=tkinter.LEFT)
 
-        lable = tkinter.Label(header_frame, text="GraphHelper", justify=tkinter.LEFT, \
-            font="14", bg="#f2f2f2")
+        lable = tkinter.Label(
+            header_frame, text="GraphHelper",
+            justify=tkinter.LEFT, font="14", bg="#f2f2f2")
         lable.pack(side=tkinter.TOP, anchor=tkinter.NW, padx=5, pady=(5,0))
-        lable = tkinter.Label(header_frame, text="Версия: "+settings.VERSION, \
+        lable = tkinter.Label(
+            header_frame, text="Версия: "+settings.VERSION,
             justify=tkinter.LEFT, bg="#f2f2f2")
         lable.pack(side=tkinter.TOP, anchor=tkinter.NW, padx=5)
 
@@ -34,41 +37,50 @@ class AboutWindow(tkinter.Toplevel):
 
         self.buttons = []
         self.keys = ["О программе", "Библиотеки", "Автор"]
-        self.dictionary = {self.keys[0]: settings.ABOUT_STRINGS[0], \
-            self.keys[1]: settings.ABOUT_STRINGS[1], self.keys[2]: \
-            settings.ABOUT_STRINGS[2]}
+        self.dictionary = {
+            self.keys[0]: settings.ABOUT_STRINGS[0],
+            self.keys[1]: settings.ABOUT_STRINGS[1],
+            self.keys[2]: settings.ABOUT_STRINGS[2]
+            }
         self._create_buttons(self.keys)
 
-        self.text_field = tkinter.Text(self, bg="#ffffff", height=10, highlightthickness=0)
+        self.text_field = tkinter.Text(
+            self, bg="#ffffff", height=10,
+            highlightthickness=0)
         self.text_field.pack(side=tkinter.TOP, fill=tkinter.X, padx=10)
         self.text_field.insert("%d.%d" % (1,0), self.dictionary[self.keys[0]])
+        self.text_field.config(state=tkinter.DISABLED)
 
         bottom_frame = tkinter.Frame(self, bg="#f2f2f2", bd=10)
         bottom_frame.pack(side=tkinter.TOP, fill=tkinter.X)
 
-        button = tkinter.Button(bottom_frame, text="Закрыть", bg="#f2f2f2", \
+        button = tkinter.Button(
+            bottom_frame, text="Закрыть", bg="#f2f2f2",
             relief="flat", highlightthickness=0, command=self.destroy)
         button.pack(side=tkinter.RIGHT)
 
     def _create_buttons(self, list_):
         for i in list_:
-            button = tkinter.Button(self.buttons_frame, text=i, bg="#f2f2f2", \
+            button = tkinter.Button(
+                self.buttons_frame, text=i, bg="#f2f2f2",
                 relief="flat", highlightthickness=0)
             button.pack(side=tkinter.LEFT)
             button.bind('<Button-1>', self._button_activated)
             if i == self.keys[0]:
-                button["bg"]="#ececec"
+                button["bg"] = "#ececec"
             self.buttons.append(button)
 
     def _button_activated(self, event):
         for i in self.buttons:
             if i is event.widget:
-                i["bg"]="#ececec"
+                i["bg"] = "#ececec"
             else:
-                i["bg"]="#f2f2f2"
+                i["bg"] = "#f2f2f2"
 
+        self.text_field.config(state=tkinter.NORMAL)
         self.text_field.delete("%d.%d" % (1,0), tkinter.END)
         self.text_field.insert("%d.%d" % (1,0), self.dictionary[event.widget["text"]])
+        self.text_field.config(state=tkinter.DISABLED)
 
 def create(arg):
     global root
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter
 import aboutwindow
 import settings
 
+
 class Main(tkinter.Frame):
 
 
@@ -11,19 +12,19 @@ class Main(tkinter.Frame):
         self.create_menu()
         self.create_toolbar()
         self.create_objectbar()
+        self.create_canvas()
 
     def create_menu(self):
         self.menu = tkinter.Menu(self, relief="flat")
         root.config(menu=self.menu)
 
         file_menu = tkinter.Menu(self.menu, tearoff=0, relief="flat")
-        file_menu.add_command(label="Создать")
         file_menu.add_command(label="Открыть...")
         file_menu.add_command(label="Сохранить...")
         file_menu.add_command(label="Выход")
 
         help_menu = tkinter.Menu(self.menu, tearoff=0, relief="flat")
-        help_menu.add_command(label="Локальная справка")
+        #help_menu.add_command(label="Локальная справка")
         help_menu.add_command(label="О программе", command=self.open_about_window)
 
         self.menu.add_cascade(label="Файл", menu=file_menu)
@@ -39,36 +40,54 @@ class Main(tkinter.Frame):
         for dict in settings.TOOLS_SETTINGS_ARRAY:
             icon = tkinter.PhotoImage(file=dict["icon"])
             self.icons.append(icon)
-            self.tool_button = tkinter.Button(toolbar, bg="#f2f2f2", bd=0, \
-                compound=tkinter.TOP, image=self.icons[i], highlightthickness=0)
+            self.tool_button = tkinter.Button(
+                toolbar, bg="#f2f2f2", bd=0,
+                compound=tkinter.TOP, image=self.icons[i],
+                highlightthickness=0)
             self.tool_button.pack(side=dict["side"])
             self.tools.append(self.tool_button)
             self.tool_button.bind('<Button-1>', self.gg)
             i+=1
 
     def create_objectbar(self):
-        objectbar = tkinter.LabelFrame(text = "Объекты:", bg = "#EBEBEB", bd=2, width = 184, relief = "flat")
-        objectbar.pack(side = tkinter.RIGHT, fill = tkinter.Y)
+        objectbar = tkinter.LabelFrame(
+            text="Объекты:", bg="#EBEBEB", bd=2,
+            width=184, relief="flat")
+        objectbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
         objectbar.pack_propagate(False)
 
-        self.scroll_canvas = tkinter.Canvas(objectbar, width=184, height = 600, bg="#EBEBEB", highlightthickness=0)
-        scroll_frame = tkinter.Frame(self.scroll_canvas, bg="#EBEBEB", width = 180)
+        self.scroll_obj_canvas = tkinter.Canvas(
+            objectbar, width=184,
+            bg="#EBEBEB", highlightthickness=0)
+        self.objectbar = tkinter.Frame(self.scroll_obj_canvas, bg="#EBEBEB", width = 180)
 
-        scrollbar = tkinter.Scrollbar(objectbar, orient="vertical",command = self.scroll_canvas.yview, relief = "flat")
-        self.scroll_canvas.configure(yscrollcommand = scrollbar.set)
+        scrollbar = tkinter.Scrollbar(
+            objectbar, orient="vertical",command=self.scroll_obj_canvas.yview,
+            relief = "flat")
+        self.scroll_obj_canvas.configure(yscrollcommand=scrollbar.set)
         scrollbar.pack(side = tkinter.RIGHT,fill = tkinter.Y)
 
-        self.scroll_canvas.create_window((0,0),window = scroll_frame, anchor='n')
-        self.scroll_canvas.pack_propagate(False)
-        self.scroll_canvas.pack(side = tkinter.RIGHT,fill = tkinter.Y)
+        self.scroll_obj_canvas.create_window((0,0),window=self.objectbar, anchor='n')
+        self.scroll_obj_canvas.pack_propagate(False)
+        self.scroll_obj_canvas.pack(side = tkinter.RIGHT, fill=tkinter.Y)
 
-        scroll_frame.bind("<Configure>", self.config_scroll_objectbar)
-		#scroll_frame.bind('<Button-1>', self.update_type)
+        self.objectbar.bind("<Configure>", self.config_scroll_objectbar)
+		#self.objectbar.bind('<Button-1>', self.update_type)
+
+    def create_canvas(self):
+        canvas = tkinter.Canvas(
+            root, bg="#ffffff", width=screen_width - 184,
+            height = screen_height - 14)
+        canvas.pack(side = tkinter.TOP, fill = tkinter.BOTH)
+
+		# self.canvas.bind('<Motion>', self.create_tip_line)
+		# self.canvas.bind('<Button-1>', self.canvas_click_left)
+		# self.canvas.bind('<Button-3>', self.canvas_click_right)
 
     def config_scroll_objectbar(self, event):
-        self.scroll_canvas
-        self.scroll_canvas
-        self.scroll_canvas.configure(scrollregion=self.scroll_canvas.bbox("all"),width=200,height=200)
+        self.scroll_obj_canvas.configure(
+            scrollregion=self.scroll_obj_canvas.bbox("all"),
+            width=200,height=200)
 
     def open_about_window(self):
         global root
@@ -90,7 +109,7 @@ if __name__ == "__main__":
     root.title("GraphHelper")
     logo = tkinter.PhotoImage(file="GraphHelper.gif")
     root.tk.call('wm', 'iconphoto', root._w, logo)
-    root.geometry(str(screen_width//2) + "x" + str(screen_height//2))
+    root.geometry(str(screen_width) + "x" + str(screen_height))
     root.minsize(screen_width//2, screen_height//2)
 
     root.mainloop()
